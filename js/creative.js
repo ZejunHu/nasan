34c684a45fd490624c06de33240c8622f6d6e3ff
Jonathan Hu <jonathanzejunhu@gmail.com>
2019-05-08 16:54:59 -0400

    complete alpha version

--- js/creative.js
+++ js/creative.js
@@ -39,6 +39,59 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
+  $('.service-trip-box-content').height($('.service-trip-box-content').first().height()+5);
+
+  //////////     For Mobile Swipe    ////////////
+  // var touchStartX = null;
+
+  // $('.carousel').each(function () {
+  //     var $carousel = $(this);
+  //     $(this).on('touchstart', function (event) {
+  //         var e = event.originalEvent;
+  //         if (e.touches.length == 1) {
+  //             var touch = e.touches[0];
+  //             touchStartX = touch.pageX;
+  //         }
+  //     }).on('touchmove', function (event) {
+  //         var e = event.originalEvent;
+  //         if (touchStartX != null) {
+  //             var touchCurrentX = e.changedTouches[0].pageX;
+  //             if ((touchCurrentX - touchStartX) > 60) {
+  //                 touchStartX = null;
+  //                 $carousel.carousel('prev');
+  //             } else if ((touchStartX - touchCurrentX) > 60) {
+  //                 touchStartX = null;
+  //                 $carousel.carousel('next');
+  //             }
+  //         }
+  //     }).on('touchend', function () {
+  //         touchStartX = null;
+  //     });
+  //   });
+//////////     For Mobile Swipe    ////////////
+
+  $('#carouselPartner').on('slide.bs.carousel', function (e) {
+
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+  });
+
+
   // Magnific popup calls
   $('#portfolio').magnificPopup({
     delegate: 'a',
